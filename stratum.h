0e3ce30b0c2a67faee3264ea0ccbe8e2b57f712d
barrystyle <barrystyle@westnet.com.au>
2019-06-04 23:09:26 +0800

    fix namespace clash

--- stratum.h
+++ stratum.h
@@ -198,7 +198,6 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/whirlpool.h"
 #include "algos/whirlpoolx.h"
 #include "algos/skein2.h"
-#include "algos/yescrypt.h"
 #include "algos/zr5.h"
 #include "algos/hive.h"
 #include "algos/sib.h"
@@ -216,3 +215,4 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/hex.h"
 #include "algos/argon2d-dyn.h"
 #include "algos/exosis.h"
+#include "algos/yespower/yespower.h"
